4f3c44d9305fd7457bec1a35a73dcf8f4b7529b8
Shubham Aggarwal <shubhamgamer.6105@gmail.com>
2025-04-21 18:17:36 +0530

    Updated App Icon

--- GUI_Code/main.cpp
+++ GUI_Code/main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    w.setWindowTitle("RiscV Simulator");
+
+    QIcon appIcon(":/images/icon.png");
+    w.setWindowIcon(appIcon);
+
     w.show();
     return a.exec();
 }
